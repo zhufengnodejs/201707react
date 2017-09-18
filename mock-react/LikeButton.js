a628f2082b6316cc5a31ea51b2e3d1382679219a
zhufengnodejs <zhufengnodejs@126.com>
2017-09-18 10:51:23 +0800

    react

--- mock-react/LikeButton.js
+++ mock-react/LikeButton.js
@@ -1,6 +1,10 @@
+/**
+ * 1.还需要我们自己手工维护DOM状态 以数据的思想去思考
+ * 2.数据改变之后还需要手工修改DOM
+ */
 class LikeButton {
   constructor(){
-    //定义一个状态
+    //如何定义组件内部的私有状态
     this.state = {liked:false};
   }
   //传入一个模板字符串，返回一个DOM对象
@@ -10,8 +14,8 @@ class LikeButton {
     //返回第一个子元素,因为只可能有一个元素，所以这样写是可以的
     return div.children[0];//<button/>
   }
+  //事件处理函数需要绑定this为组件实例
   handleClick(event){
-    console.log(this);
     let likeText = document.querySelector('#like-text');
     this.state.liked  = !this.state.liked;
     likeText.innerHTML = this.state.liked?'取消':'点赞';
@@ -23,7 +27,7 @@ class LikeButton {
     this.ele = this.createDOMFromString(
       `
        <button id="like-button">
-         <span id="like-text">点赞</span>
+         <span id="like-text" style="color:red">点赞</span>
        </button>
       `);
     this.ele.addEventListener('click',this.handleClick.bind(this));
