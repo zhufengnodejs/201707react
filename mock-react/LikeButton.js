cbd36a4c00670db1437f0d7bae42b6d1a815ed8c
zhufengnodejs <zhufengnodejs@126.com>
2017-09-18 10:20:21 +0800

    react

---
+++ mock-react/LikeButton.js
@@ -0,0 +1,12 @@
+class LikeButton {
+  //render方法的返回值决定了此组件长什么样子
+  render(){
+    return (
+      `
+       <button id="like-button">
+         <span id="like-text">赞+1</span>
+       </button>
+      `
+    )
+  }
+}
