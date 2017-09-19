cf859febf210a4c097f112ac9ba51c2395180c18
zhufengnodejs <zhufengnodejs@126.com>
2017-09-19 15:14:28 +0800

    messagelist

--- message/index.js
+++ message/index.js
@@ -1,2 +1,5 @@
-
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import {render} from 'react-dom';
+import MessageApp from "./components/MessageApp";
+render(<MessageApp/>,document.querySelector('#root'));
