6fb8dd96a4fd80e6eecfaf2d50cd2010a066f8cf
zhufengnodejs <zhufengnodejs@126.com>
2017-09-19 15:33:04 +0800

    mkist

--- message/components/MessageApp.js
+++ message/components/MessageApp.js
@@ -1,8 +1,33 @@
 import React,{Component} from 'react';
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
 export default class MessageApp extends Component{
+  constructor(){
+    super();
+    this.state = {messages:[
+      {id:1,content:'今天天气真好!',author:'张三',createAt:new Date()},
+      {id:2,content:'是的，对的!',author:'李四',createAt:new Date()}
+    ]};
+  }
   render(){
     return (
-      <div></div>
+      <div className="container" style={{marginTop:'20px'}}>
+        <div className="row" >
+          <div className="col-md-8 col-md-offset-2">
+            <div className="panel panel-default">
+              <div className="panel-heading">
+                <h4 className="text-center">欢迎光临珠峰留言板</h4>
+              </div>
+              <div className="panel-body">
+                <MessageList messages={this.state.messages}/>
+              </div>
+              <div className="panel-footer">
+                <MessageForm/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- message/components/MessageList.js
+++ message/components/MessageList.js
@@ -2,7 +2,13 @@ import React,{Component} from 'react';
 export default class MessageList extends Component{
   render(){
     return (
-      <div></div>
+      <ul className="list-group">
+        {
+          this.props.messages.map((item,index)=>(
+            <li key={item.id} className="list-group-item">{item.author}:{item.content} <span className="pull-right">{item.createAt.toLocaleString()}</span></li>
+          ))
+        }
+      </ul>
     )
   }
 }
