89c384f2be2271a92612d88824c87a699668b054
zhufengnodejs <zhufengnodejs@126.com>
2017-09-19 11:22:20 +0800

    配置webpack

--- src/includes/8.props.js
+++ src/includes/8.props.js
@@ -8,10 +8,28 @@
  * 2.属性和状态的区别和联系
  * 3.单向数据流
  *
+ * 1. 属性和状态都可以作为组件数据源
+ * 2. 属性是外界传入的，内部不能改。状态是内部初始化的，可以修改。
+ * 3. 状态改变后要会得新刷新视图
+ * 4. 如果在子组件里要改变父组件的状态，需要在父组件里定义一个改变父组件状态的方法，然后作为属性传递给子组件，子组件在特定的时刻调用即可。
+ * 5. 可以在组件内定义默认属性对象，这样的话在父组件没有传递属性的时候就可以使用默认对象。
+ * 6. 在组件内部可以定义propTypes,规定父组件使用子组件的时候需要传递参数的名称和类型。
+ *
  */
 import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 class LikeButton extends Component{
+  //组件的默认属性对象 静态的-这个属性是属于类的
+  static defaultProps = {
+    text:'默认状态'
+  }
+  //类似于一个接口文档，用来告诉父组件应该传入什么参数，什么类型
+  static propTypes = {
+    text:PropTypes.string,
+    //isRequired必传，如果不传会报错，如果传了但类型不对也会报错
+    age:PropTypes.number.isRequired
+  }
   render(){
     /**
      * onClick等于的是一个函数定义，而非一个函数执行结果
@@ -38,7 +56,7 @@ class Index extends Component{
   render(){
     return (
       <div>
-        <LikeButton text={this.state.text} changeText={this.changeText}/>
+        <LikeButton age={100} text={this.state.text}  changeText={this.changeText}/>
       </div>
     )
   }
