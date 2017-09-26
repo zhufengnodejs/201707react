370c0dea2706cc22a083d56a112b0f6ed3cfd1f7
zhufengnodejs <zhufengnodejs@126.com>
2017-09-26 12:48:12 +0800

    评论的持久化

--- message/components/Message.js
+++ message/components/Message.js
@@ -2,7 +2,9 @@ import React,{Component} from 'react';
 export default class Message extends Component{
   render(){
     return (
-      <div></div>
+      <li key={this.props.item.id} className="list-group-item">{this.props.item.author}:{this.props.item.content}
+        <button onClick={()=>this.props.delMessage(this.props.item.id)} className="btn btn-danger btn-xs">删除</button>
+        <span className="pull-right">{this.props.item.createAt.toLocaleString()}</span></li>
     )
   }
 }
--- message/components/MessageList.js
+++ message/components/MessageList.js
@@ -1,13 +1,12 @@
 import React, {Component} from 'react';
+import Message from "./Message";
 export default class MessageList extends Component {
   render() {
     return (
       <ul className="list-group">
         {
           this.props.messages.map((item, index) => (
-            <li key={item.id} className="list-group-item">{item.author}:{item.content}
-              <button onClick={()=>this.props.delMessage(item.id)} className="btn btn-danger btn-xs">删除</button>
-              <span className="pull-right">{item.createAt.toLocaleString()}</span></li>
+             <Message item={item} delMessage={this.props.delMessage}/>
           ))
         }
       </ul>
