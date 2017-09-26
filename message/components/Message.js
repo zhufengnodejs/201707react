37800afdc4132fa162931755769a17e15fdfdc3b
zhufengnodejs <zhufengnodejs@126.com>
2017-09-26 15:18:47 +0800

    评论的持久化

--- message/components/Message.js
+++ message/components/Message.js
@@ -2,9 +2,20 @@ import React,{Component} from 'react';
 export default class Message extends Component{
   constructor(){
     super();
-    this.state = {time:''};
+    this.state = {time:'',filteredContent:''};
   }
   componentWillMount(){
+    this.getTime();
+    this.getContent();
+    this.timer = setInterval(this.getTime,5000);
+  }
+  getContent = ()=>{
+    let content = this.props.item.content;
+    // I like `js` very much .  =>   `js` <code>js</code>
+    let filteredContent = content.replace(/`([\s\S]+)`/,'<code>$1</code>');
+    this.setState({filteredContent});
+  }
+  getTime = ()=>{
     let createAt = this.props.item.createAt;//创建时间
     let duration = (Date.now() - new Date(createAt))/1000;
     let time;
@@ -13,11 +24,11 @@ export default class Message extends Component{
     }else{
       time = duration<1?'刚刚':`${Math.round(duration)}秒以前`;
     }
-    this.setState({time})
+    this.setState({time});
   }
   render(){
     return (
-      <li key={this.props.item.id} className="list-group-item">{this.props.item.author}:{this.props.item.content}
+      <li key={this.props.item.id} className="list-group-item">{this.props.item.author}:{this.state.filteredContent}
         <button onClick={()=>this.props.delMessage(this.props.item.id)} className="btn btn-danger btn-xs">删除</button>
         <span className="pull-right">{this.state.time}</span></li>
     )
