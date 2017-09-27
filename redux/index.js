6727d9f98e3b589450806fb498e44d4c0dd92fcf
zhufengnodejs <zhufengnodejs@126.com>
2017-09-27 10:03:20 +0800

    redux

--- redux/index.js
+++ redux/index.js
@@ -1,3 +1,24 @@
-/**
- * 珠峰培训 http://www.zhufengpeixun.cn
- */
+let appState = {
+  fontSize:'16px',
+  title:{
+    text:'标题',
+    color:'red'
+  },
+  content:{
+    text:'内容',
+    color:'green'
+  }
+}
+function renderTitle(title){
+  let titleEle = document.querySelector('#title');
+  titleEle.innerHTML = title.text;
+  titleEle.style.color = title.color;
+}
+function renderContent(content){
+
+}
+function renderApp(appState){
+  renderTitle(appState.title);
+  renderContent(appState.content);
+}
+renderApp(appState);
