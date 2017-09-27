a8478b20461544a9ee99aad86de267888c295262
zhufengnodejs <zhufengnodejs@126.com>
2017-09-27 10:28:51 +0800

    redux

--- redux/index.js
+++ redux/index.js
@@ -9,16 +9,35 @@ let appState = {
     color:'green'
   }
 }
-function renderTitle(title){
+/**
+ * 1.我规定一个专门的修改数据的方法，如果想修改数据只能走这一个方法。
+ * dispatch 派发 派遣
+ * action 代表一个命令对象，动作对象，它就是一个普通JS对象,起码需要一个字段，type=命令的类型,其它字段随意
+ */
+const CHANGE_FONT_SIZE = 'CHANGE_FONT_SIZE'
+let dispatch = (action)=>{
+  switch(action.type){
+    case CHANGE_FONT_SIZE:
+      appState.fontSize = action.fontSize;
+    default:
+      return;
+  }
+}
+function renderTitle(){
   let titleEle = document.querySelector('#title');
-  titleEle.innerHTML = title.text;
-  titleEle.style.color = title.color;
+  titleEle.innerHTML = appState.title.text;
+  titleEle.style.color = appState.title.color;
+  titleEle.style.fontSize = appState.fontSize;
 }
-function renderContent(content){
-
+function renderContent(){
+  let contentEle = document.querySelector('#content');
+  contentEle.innerHTML = appState.content.text;
+  contentEle.style.color = appState.content.color;
+  contentEle.style.fontSize = appState.fontSize;
 }
-function renderApp(appState){
-  renderTitle(appState.title);
-  renderContent(appState.content);
+function renderApp(){
+  renderTitle();
+  renderContent();
 }
-renderApp(appState);
+dispatch({type:CHANGE_FONT_SIZE,fontSize:'50px'})
+renderApp();
