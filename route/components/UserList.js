4af2f3e73c44282225565b0708f5304ec42438cb
zhufengnodejs <zhufengnodejs@126.com>
2017-10-10 17:00:12 +0800

    路由

--- route/components/UserList.js
+++ route/components/UserList.js
@@ -1,10 +1,30 @@
-import React,{Component} from 'react';
-export default class UserList extends Component{
-    render(){
-        return (
-            <div>
-              UserList
-            </div>
-        )
-    }
+import React, {Component} from 'react';
+export default class UserList extends Component {
+  constructor() {
+    super();
+    this.state = {users: []};
+  }
+
+  componentWillMount() {
+    let users = JSON.parse(localStorage.getItem('USERS') || "[]");
+    this.setState({users});
+  }
+
+  render() {
+    return (
+      <ul className="list-group">
+        <li className="list-group-item">
+          用户名
+        </li>
+        {
+          this.state.users.map((item,index)=>(
+            <li className="list-group-item">
+              {item.username}
+            </li>
+          ))
+        }
+
+      </ul>
+    )
+  }
 }
