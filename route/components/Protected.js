e264e6e3cc24299990124d7dae0e25ce82007450
zhufengnodejs <zhufengnodejs@126.com>
2017-10-10 18:05:15 +0800

    路由

--- route/components/Protected.js
+++ route/components/Protected.js
@@ -5,8 +5,9 @@ import {Route, Redirect} from 'react-router-dom';
 //再把other对象的全部属性取出来赋给Route
 // component=组件
 // render函数 当路由匹配的时候，渲染的是render方法的返回值
-export default function ({Component: _comp, path}) {
-  return <Route path={path} render={
+export default function ({component: _comp, ...rest}) {
+  return <Route {...rest} render={
     props => localStorage.getItem('login') ? <_comp/> : <Redirect to="/login" state={{from: props.location.pathname}}/>
   }/>
+  return null;
 }
