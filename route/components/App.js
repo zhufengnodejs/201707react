0883268a4bb6eb8751bdcd6a43164a198a24adc2
zhufengnodejs <zhufengnodejs@126.com>
2017-10-10 15:54:38 +0800

    路由

--- route/components/App.js
+++ route/components/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
 //Router 容器，它是用来包裹路由规则的
 //Route 是路由规则
-import {HashRouter as Router, Route} from 'react-router-dom';
-import Header from "../../src/includes/context/Header";
+//BrowserRouter as Router, HTML5  history api
+import {HashRouter as Router, Route,Link} from 'react-router-dom';
 import Home from "./Home";
 import User from "./User";
 import Profile from "./Profile";
@@ -10,16 +10,25 @@ export default class App extends Component {
   render() {
     return (
       <Router>
-        <div>
-          <ul>
-            <li><a href="#/">首页</a></li>
-            <li><a href="#/user">用户管理</a></li>
-            <li><a href="#/profile">个人中心</a></li>
-          </ul>
-          <div>
-            <Route path="/" component={Home}/>
-            <Route path="/user" component={User}/>
-            <Route path="/profile" component={Profile}/>
+        <div className="container">
+          <nav className="navbar navbar-default">
+              <div className="container-fluid">
+                <div className="navbar-header">
+                  <a href="#/" className="navbar-brand">用户管理</a>
+                </div>
+                <ul className="nav navbar-nav">
+                  <li><Link to="/">首页</Link></li>
+                  <li><Link to="/user">用户管理</Link></li>
+                  <li><Link to="/profile">个人中心</Link></li>
+                </ul>
+              </div>
+          </nav>
+          <div className="row">
+            <div className="col-sm-12">
+              <Route exact path="/" component={Home}/>
+              <Route path="/user" component={User}/>
+              <Route path="/profile" component={Profile}/>
+            </div>
           </div>
         </div>
       </Router>
