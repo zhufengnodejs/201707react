3bc4602385adf78580adf9b226e5409c2f8a5a0d
zhufengnodejs <zhufengnodejs@126.com>
2017-10-10 17:35:36 +0800

    路由

--- route/components/App.js
+++ route/components/App.js
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 //Router 容器，它是用来包裹路由规则的
 //Route 是路由规则
 //BrowserRouter as Router, HTML5  history api
-import {HashRouter as Router, Route,Link} from 'react-router-dom';
+import {HashRouter as Router, Route,Link,NavLink} from 'react-router-dom';
 import Home from "./Home";
 import User from "./User";
 import Profile from "./Profile";
 export default class App extends Component {
   render() {
+    let activeStyle = {color:'red'};
     return (
       <Router>
         <div className="container">
@@ -17,9 +18,9 @@ export default class App extends Component {
                   <a href="#/" className="navbar-brand">用户管理</a>
                 </div>
                 <ul className="nav navbar-nav">
-                  <li><Link to="/">首页</Link></li>
-                  <li><Link to="/user">用户管理</Link></li>
-                  <li><Link to="/profile">个人中心</Link></li>
+                  <li><NavLink exact activeStyle={activeStyle} to="/">首页</NavLink></li>
+                  <li><NavLink activeStyle={activeStyle} to="/user">用户管理</NavLink></li>
+                  <li><NavLink activeStyle={activeStyle} to="/profile">个人中心</NavLink></li>
                 </ul>
               </div>
           </nav>
--- route/components/User.js
+++ route/components/User.js
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
-import {Link, Route} from 'react-router-dom';
+import {Link, Route,NavLink} from 'react-router-dom';
 import UserList from "./UserList";
 import UserAdd from "./UserAdd";
 import UserDetail from "./UserDetail";
 export default class User extends Component {
   render() {
+    let activeStyle = {color:'red'};
     return (
       <div className="row">
         <div className="col-sm-3">
           <ul className="nav nav-stacked" style={{textAlign:'center'}}>
-            <li><Link to="/user/list">用户列表</Link></li>
-            <li><Link to="/user/add">添加用户</Link></li>
+            <li><NavLink activeStyle={activeStyle} to="/user/list">用户列表</NavLink></li>
+            <li><NavLink activeStyle={activeStyle} to="/user/add">添加用户</NavLink></li>
           </ul>
         </div>
         <div className="col-sm-9">
