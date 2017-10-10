39cfafc1718a096d9225a05416969c4a2d149b43
zhufengnodejs <zhufengnodejs@126.com>
2017-10-10 16:15:08 +0800

    路由

--- route/components/User.js
+++ route/components/User.js
@@ -1,10 +1,22 @@
-import React,{Component} from 'react';
-export default class User extends Component{
-    render(){
-        return (
-            <div>
-              User
-            </div>
-        )
-    }
+import React, {Component} from 'react';
+import {Link, Route} from 'react-router-dom';
+import UserList from "./UserList";
+import UserAdd from "./UserAdd";
+export default class User extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-sm-3">
+          <ul className="nav nav-stacked">
+            <li><Link to="/user/list">用户列表</Link></li>
+            <li><Link to="/user/add">添加用户</Link></li>
+          </ul>
+        </div>
+        <div className="col-sm-9">
+          <Route path="/user/list" component={UserList}/>
+          <Route path="/user/add" component={UserAdd}/>
+        </div>
+      </div>
+    )
+  }
 }
---
+++ route/components/UserAdd.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+export default class UserAdd extends Component{
+    render(){
+        return (
+            <div>
+              UserAdd
+            </div>
+        )
+    }
+}
